944495bf2549fb59c565b843f81ebed95bf665c7
Cerenda5 <linda.dogan@outlook.fr>
2020-12-06 13:06:33 +0100

    Like feature, model, schema, resolver

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import resolvers from './resolvers'
 
 import userModel from './models/userModel';
 import postModel from './models/postModel';
+import likeModel from './models/likeModel';
 
 dotenv.config({
     path: `./.env.${process.env.NODE_ENV}`
@@ -40,7 +41,8 @@ const server = new ApolloServer({
                 userInfo,
                 models: {
                     userModel,
-                    postModel
+                    postModel,
+                    likeModel,
                 },
             };
         }
--- src/schemas/likeSchema.js
+++ src/schemas/likeSchema.js
@@ -1,13 +1,19 @@
 import { gql } from 'apollo-server';
 
-
 export default  gql`
-
   type Like {
     id: ID!
     post: ID
-    user: ID
+    author: ID
   }
+
+  extend type Query {
+    # Get user Like
+    like(id: ID!): [Like]!
+    
+    #Get User Likes
+    likes: [Like]!
+}
 
   extend type Mutation {
     createLike(userId: ID!,postId: ID!): Like
