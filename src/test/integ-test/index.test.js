6ca0e582402c0865e8b655ec9a125e9c3eea43ac
BuyukCem <cembuyuk7@gmail.com>
2021-01-16 22:12:32 +0100

    temps commit

---
+++ src/test/integ-test/index.test.js
@@ -0,0 +1,36 @@
+const {gql} = require('apollo-server-express');
+const {appServ, server, mongodbconfig} = require('../../index')
+const supertest = require("supertest");
+const {connectToDb, closeDbConnection, removeAllCollections} = require("./test-setup-bdd");
+const request = supertest(appServ)
+
+const {userRegister, userAuth} = require('./auth.test')
+const {post} = require('./post.test')
+const {comment} = require('./comment.test')
+
+describe('Init Test', ()=>{
+
+    // Connect to Mongoose
+    beforeAll(async () => {
+        await connectToDb()
+        await removeAllCollections()
+    })
+
+    // Cleans up database between each test
+    afterEach(async () => {
+    })
+
+    // Disconnect Mongoose
+    afterAll(async () => {
+        await closeDbConnection()
+        appServ.close()
+        process.exit()
+    })
+
+    describe('User register', ()=>{userRegister(request)})
+    describe('User Auth', ()=>{userAuth(request)})
+    describe('Post', ()=>{post(request)})
+    describe('Comment', ()=>{comment(request)})
+
+})
+
