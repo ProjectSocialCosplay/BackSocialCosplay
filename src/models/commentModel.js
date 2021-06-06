13d5d91646f4f9399555e9426b298f4715f26ad9
BuyukCem <cembuyuk7@gmail.com>
2021-06-06 13:11:50 +0200

    commit

--- src/models/commentModel.js
+++ src/models/commentModel.js
@@ -1,6 +1,4 @@
-const {mongoose} = require('mongoose');
-const Schema = mongoose.Schema;
-
+const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
     comment: {
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt');
-const {mongoose} = require('mongoose');
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const uniqueValidator = require('mongoose-unique-validator');
 
