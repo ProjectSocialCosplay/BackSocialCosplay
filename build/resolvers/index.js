fbe576665ab9e7a3bcbfe225a6cde5343489f7b3
BuyukCem <cembuyuk7@gmail.com>
2021-06-06 15:28:14 +0200

    commit

--- build/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-import userResolvers from './userResolvers';
-import postResolver from "./postResolvers";
-import likeResolvers from './likeResolvers';
-import commentResolvers from './commentResolvers';
-import pictureResolvers from "./pictureResolvers";
-import followResolvers from "./followResolvers";
-
-export default [userResolvers, postResolver, commentResolvers, likeResolvers, pictureResolvers, followResolvers];
-//# sourceMappingURL=index.js.map
--- build/test/context.js
+++
@@ -1,7 +0,0 @@
-function IntegTestDataUserOne() {}
-let userOne = new IntegTestDataUserOne();
-let userTwo = new IntegTestDataUserOne();
-
-exports.IntegTestDataUserOne = userOne;
-exports.IntegTestDataUserTwo = userTwo;
-//# sourceMappingURL=context.js.map
